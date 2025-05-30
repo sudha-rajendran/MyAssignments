f8723474a8ff3450e6632f92b0cdfdabc0453ad1
sudha-rajendran <sudha.asp12@gmail.com>
2025-05-30 20:22:16 +0530

    Add files via upload

---
+++ day2/LearnElseIfCondition.java
@@ -0,0 +1,30 @@
+package week1.day2;
+
+public class LearnElseIfCondition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=7;
+		int b=7;
+		
+		if(a<b) {
+			System.out.println(a+" is smaller than "+b);
+			
+		}
+		
+		else if(a>b) {
+			System.out.println(a+" is greater than "+b);
+		}
+		
+		else if(a==b) {
+			System.out.println(a+" is equal to "+b);
+		}
+		
+		//else {
+			//System.out.println(a+" is equal to "+b);
+		
+		
+	}
+
+}
